24c5184e40be23022603ac0b81f3ec4612499905
Chrille313 <hjerpe26@gmail.com>
2025-10-30 14:51:22 +0100

    Add files via upload

---
+++ Raspberry_code/main.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    # Setup
+
+    while True:
+        # Read sensor data
+
+        # Analyze sensor data
+
+        # Kinematics
+        x = f()
+
+        # Calculcate control signal
+
+        # Send control signal
+
