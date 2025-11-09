ec1db2f1817eb93b74a438792be7f4a36fc8ad09
eliasalbag <eliasalbag97@gmail.com>
2025-11-09 23:37:46 +0100

    The intention is doing the parameter optimizer using the toolbox to match the data saved from the real excavator run. Bom Stick and Bucket initial angles are now the same as they were in the physical run. (I had a lot of problems fixing the bucket. but in the end it worked...). Parameters to be optimized are defined in the startup script. Todo: make sure the input behaves the same way as in the physical machine. Also make sure the size of the input and output of the data from the physical run are the same. Then, do the optimizer.

--- Scripts_Data/Excavator_Parameters.m
+++ Scripts_Data/Excavator_Parameters.m
@@ -1,4 +1,3 @@
-clear all, close all, clc
 %Dimensions in m
 
 %Links
@@ -18,17 +17,21 @@ Excavator_Param.Base.CutoutWidth = 25e-3;
 Excavator_Param.Base.Width = 250e-3;
 Excavator_Param.Base.InnerWidth = Excavator_Param.Base.Width - 2*Excavator_Param.Base.CutoutWidth;
 
+
 %Bom
 Excavator_Param.Bom.Width = 200e-3;
 Excavator_Param.Bom.CutoutWidth = 25e-3;
 Excavator_Param.Bom.InnerWidth = Excavator_Param.Bom.Width - 2*Excavator_Param.Bom.CutoutWidth;
 Excavator_Param.Bom.Length = 1903.392e-3;
+Excavator_Param.Bom.Weight = 200;%[kg]
 
 %Stick
 Excavator_Param.Stick.Width = 150e-3;
 Excavator_Param.Stick.CutoutWidth = 25e-3;
 Excavator_Param.Stick.InnerWidth = Excavator_Param.Stick.Width - 2*Excavator_Param.Stick.CutoutWidth;
 Excavator_Param.Stick.Length = 1351.485e-3;
+Excavator_Param.Stick.Weight = 150;%[kg]
+
 
 %Linkage arms
 Excavator_Param.LinkageArm.Width = 30e-3;
@@ -43,8 +46,30 @@ Excavator_Param.LinkageArmLong.CutoutWidth = 25e-3;
 %Bucket
 Excavator_Param.Bucket.ThinWidth = 25e-3;
 Excavator_Param.Bucket.Length = 590.267e-3;
-
 Excavator_Param.Link.BucketTopRadius = 15e-3;
 Excavator_Param.Link.BucketTopLength = 2*(Excavator_Param.Bucket.ThinWidth) + (Excavator_Param.Stick.Width);
+Excavator_Param.Bucket.Weight = 70; %[kg]
 
 
+
+
+
+
+%Joints
+Excavator_Param.Joint1.Damping = 100;
+Excavator_Param.Joint2.Damping = 100;
+
+%Hydraulics
+Excavator_Param.Cylinder1.Damping = 1000;
+Excavator_Param.Cylinder1.PistonCrossSectionalAreaChamberA = 0.125;
+Excavator_Param.Cylinder1.PistonCrossSectionalAreaChamberB = 0.125;
+Excavator_Param.Cylinder1.PistonStroke = 580;
+Excavator_Param.Cylinder1.DeadVolumeChamberA = 0.05;
+Excavator_Param.Cylinder1.DeadVolumeChamberB = 0.05;
+
+Excavator_Param.Cylinder2.Damping = 1000;
+Excavator_Param.Cylinder2.PistonCrossSectionalAreaChamberA = 0.125;
+Excavator_Param.Cylinder2.PistonCrossSectionalAreaChamberB = 0.125;
+Excavator_Param.Cylinder2.PistonStroke = 580;
+Excavator_Param.Cylinder2.DeadVolumeChamberA = 0.05;
+Excavator_Param.Cylinder2.DeadVolumeChamberB = 0.05;
