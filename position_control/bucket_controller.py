e5a7e9232fd376149d41f310897dc8b21bde58d8
viktortraktor-volvo <viktortraktorvolvo@gmail.com>
2025-11-19 17:05:04 +0100

    add working bucket controll (untuned) + wip pointplanner

--- position_control/bucket_controller.py
+++ position_control/bucket_controller.py
@@ -1,18 +1,28 @@
 import numpy as np
-
+from numpy import sign
 
 #### TUNING PARAMETERS
-
+degpersec = 10
 Ts = 0.01
+
+BUCKET_STICK1 = 16
+BUCKET_STICK2 = 32
+if flag is None:
+    flag = 1
 
 ## Bucket Control Parameters
 Kp_bucket = 30
-Ki_bucket = 3
-Kd_bucket = 1
+Ki_bucket = 1
+Kd_bucket = 0
 
 
 ## Bucket controller
 calc_thetaw = theta1 + theta2 + theta3
+
+
+if theta_ref is None:
+    theta_ref = -20
+
 
 bucket_error = theta_ref - calc_thetaw
 
@@ -30,4 +40,25 @@ bucket_error_prev = bucket_error
 # calculate derivative
 bucket_error_derivative = (bucket_error - bucket_error_prev) / Ts
 
-control_3 = -(Kp_bucket * bucket_error +  Ki_bucket * bucket_error_integral +  Kd_bucket * bucket_error_derivative - bom_input*0.4 - stick_input*0.4)
+control_3 = -(Kp_bucket * bucket_error +  Ki_bucket * bucket_error_integral +  Kd_bucket * bucket_error_derivative )
+
+
+
+if bucket_stick_signal == 0 and flag == 0:
+    theta_ref = calc_thetaw
+    bucket_error_integral_prev = 0
+    bucket_error_prev = 0
+    flag = 1
+    
+
+if bucket_stick_signal == BUCKET_STICK1 or bucket_stick_signal == BUCKET_STICK2:
+    if bucket_stick_signal == BUCKET_STICK1 + BUCKET_STICK2:
+        pass
+    else:
+
+        flag = 0
+
+        if bucket_stick_signal == BUCKET_STICK1:
+            theta_ref -= degpersec * Ts 
+        elif bucket_stick_signal == BUCKET_STICK2:
+            theta_ref += degpersec * Ts 
--- position_control/point_planner.py
+++ position_control/point_planner.py
@@ -3,56 +3,38 @@ Point planner for 2DOF robotic arm.
 Calculates (x_ref, y_ref) based on joystick inputs and current position.
 """
 
+from numpy import arctan2, cos, sin
 
-from numpy import arctan, cos, sin, sign
 L1 = 1903.392e-3
 L2 = 1351.485e-3
 
-"""
-Calculate current (x,y) position based on measured joint angles
-"""
-
+# Current end-effector (x, y)
 y_meas = L1*sin(theta1) + L2*sin(theta1 + theta2)
 x_meas = L1*cos(theta1) + L2*cos(theta1 + theta2)
 
 tol = 20
-increment = 10e-2 # 10 cm increment
-point_tolerance = 5
+increment = 0.20       # 20 cm
+point_tolerance = 0.10 # 10 cm
 
-assert increment > point_tolerance
-"""
-Check sticks
-"""
+# Reset reference
+if reset == 1:
+    x_ref = 2.0
+    y_ref = 0.5
 
-if abs(x_stick) > tol:
-    dx = increment * cos(arctan(y_stick/x_stick))
-else:
-    dx = 0
+# Only add a new point when we're close to the previous one
+if increment > point_tolerance:
 
+    # --- X DIRECTION ---------------------------------------------------------
+    if abs(x_ref - x_meas) < point_tolerance:
+        if abs(x_stick) > tol or abs(y_stick) > tol:
+            angle = arctan2(y_stick, x_stick)
+            x_ref = x_meas + increment * cos(angle)
 
-if abs(y_stick) > tol:
-    dy = increment * sin(arctan(y_stick/x_stick))
-else:
-    dy = 0
-
-
-"""
-Calculate new (x_ref, y_ref) position
-"""
-
-if x_ref or y_ref is None:
-    x_ref = x_meas
-    y_ref = y_meas
-
-
-if abs(x_ref - x_meas) < point_tolerance:
-    x_ref = x_meas + dx
-
-
-if abs(y_ref - y_meas) < point_tolerance:
-    y_ref = y_meas + dy
-
-
+    # --- Y DIRECTION ---------------------------------------------------------
+    if abs(y_ref - y_meas) < point_tolerance:
+        if abs(x_stick) > tol or abs(y_stick) > tol:
+            angle = arctan2(y_stick, x_stick)
+            y_ref = y_meas + increment * sin(angle)
 
 x_ref_out = x_ref
 y_ref_out = y_ref
